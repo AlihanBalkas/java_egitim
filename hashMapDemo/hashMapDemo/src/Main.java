ca717850b6356bea766cb5c0035c882cd5bc2744
Bifourz <113182096+Bifourz@users.noreply.github.com>
2022-09-23 16:35:39 +0300

    egitim

--- hashMapDemo/hashMapDemo/src/Main.java
+++ hashMapDemo/hashMapDemo/src/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         HashMap<String,String> sozluk = new HashMap<String,String>();
-        sozluk.put( )
+        sozluk.put("book","kitap");
+        sozluk.put("table","masa");
+        sozluk.put("computer","bilgisayar");
+        System.out.println(sozluk.size());// sözlükteki eleman sayısını verir
+
+        for (String item : sozluk.keySet()){ // keySet= liste olarak verir
+            System.out.println("Eleman-" +item+" Değer-"+sozluk.get(item));
+        }
+
+
+        sozluk.remove("table");
+        sozluk.clear();
+        System.out.println(sozluk.get("table"));
 
     }
 }
