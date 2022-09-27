46a9e49e061916e2bc71eca4d880ee04632d1fba
Bifourz <113182096+Bifourz@users.noreply.github.com>
2022-09-27 13:37:38 +0300

    javaEgitim

---
+++ readingFileDemo/readingFileDemo/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = null;
+        int total = 0;
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\zzq70\\OneDrive\\Masaüstü\\git\\java_egitim\\readingFileDemo\\readingFileDemo\\src\\sayilar"));
+            String line = null;
+            while ((line = reader.readLine()) !=null){
+                total +=Integer.valueOf(line);
+            }
+            System.out.println("Toplam = "+total);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            reader.close();
+        }
+
+    }
+}
